8c74528bb6e1e78e025e3b1597614ba888040d84
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-08 10:28:01 +0200

    Add Coach and Formation

---
+++ src/hr/fer/oop/lab3/Coach.java
@@ -0,0 +1,40 @@
+package hr.fer.oop.lab3;
+
+public class Coach extends Person {
+
+    private int coachingSkill;
+
+    private Formation formation;
+
+    public Coach(String name, String country) {
+        super(name, country);
+        coachingSkill = 50;
+        formation = Formation.F442();
+    }
+
+    public Coach(String name, String country, int emotion, int coachingSkill, Formation formation) {
+        super(name, country, emotion);
+        setCoachingSkill(coachingSkill);
+        this.formation = formation;
+    }
+
+    public int getCoachingSkill() {
+        return coachingSkill;
+    }
+
+    public void setCoachingSkill(int coachingSkill) {
+        if (coachingSkill < 0 || coachingSkill > 100) {
+            System.out.println("Cannot set coachingSkill to: " + coachingSkill);
+            return;
+        }
+        this.coachingSkill = coachingSkill;
+    }
+
+    public Formation getFormation() {
+        return formation;
+    }
+
+    public void setFormation(Formation formation) {
+        this.formation = formation;
+    }
+}
---
+++ src/hr/fer/oop/lab3/Formation.java
@@ -0,0 +1,22 @@
+package hr.fer.oop.lab3;
+
+public class Formation {
+
+    private static Formation f442 = new Formation();
+
+    private static Formation f352 = new Formation();
+
+    private static Formation f541 = new Formation();
+
+    public static Formation F442() {
+        return f442;
+    }
+
+    public static Formation F352() {
+        return f352;
+    }
+
+    public static Formation F541() {
+        return f541;
+    }
+}
