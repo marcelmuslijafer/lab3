fa07b35bb57cf7f57527cc1542c11af0e95a59d8
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-09 15:49:09 +0200

    Add javadoc

--- src/hr/fer/oop/lab3/Formation.java
+++ src/hr/fer/oop/lab3/Formation.java
@@ -1,21 +1,42 @@
 package hr.fer.oop.lab3;
 
+/**
+ * Class that describes a formation.
+ */
 public class Formation {
 
+    /**
+     * 4-4-2 formation
+     */
     private static Formation f442 = new Formation();
 
+    /**
+     * 3-5-2 formation
+     */
     private static Formation f352 = new Formation();
 
+    /**
+     * 5-4-1 formation
+     */
     private static Formation f541 = new Formation();
 
+    /**
+     * @return the 4-4-2 formation
+     */
     public static Formation F442() {
         return f442;
     }
 
+    /**
+     * @return the 3-5-2 formation
+     */
     public static Formation F352() {
         return f352;
     }
 
+    /**
+     * @return the 5-4-1 formation
+     */
     public static Formation F541() {
         return f541;
     }
--- src/hr/fer/oop/lab3/PlayingPosition.java
+++ src/hr/fer/oop/lab3/PlayingPosition.java
@@ -1,8 +1,26 @@
 package hr.fer.oop.lab3;
 
+/**
+ * An enumeration of positions on the pitch.
+ */
 public enum PlayingPosition {
+    /**
+     * Goalkeeper.
+     */
     GK,
+
+    /**
+     * Defender
+     */
     DF,
+
+    /**
+     * Midfielder
+     */
     MF,
+
+    /**
+     * Forward
+     */
     FW
 }
