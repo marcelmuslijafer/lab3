a33b5c6881625f28e25e74b100002f0dcc142336
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-09 15:45:18 +0200

    Add javadoc

--- src/hr/fer/oop/lab3/Coach.java
+++ src/hr/fer/oop/lab3/Coach.java
@@ -1,17 +1,40 @@
 package hr.fer.oop.lab3;
 
+/**
+ * Class that describes a Coach.
+ */
 public class Coach extends Person {
 
+    /**
+     * Coach's skill. Values must be in the range [0, 100].
+     */
     private int coachingSkill;
 
+    /**
+     * Coach's favorite formation to play.
+     */
     private Formation formation;
 
+    /**
+     * Constructs a coach with the specified name and country.
+     * emotion, coachingSkill and formation are default. (50, 50, F442)
+     * @param name coach's name
+     * @param country coach's country
+     */
     public Coach(String name, String country) {
         super(name, country);
         coachingSkill = 50;
         formation = Formation.F442();
     }
 
+    /**
+     * Constructs a coach with the specified name, country, emotion, coachingSkill and formation.
+     * @param name coach's name
+     * @param country coach's country
+     * @param emotion coach's emotion
+     * @param coachingSkill coach's skill
+     * @param formation coach's favorite formation
+     */
     public Coach(String name, String country, int emotion, int coachingSkill, Formation formation) {
         super(name, country, emotion);
         this.coachingSkill = 50;
@@ -19,10 +42,17 @@ public class Coach extends Person {
         this.formation = formation;
     }
 
+    /**
+     * @return coach's skill
+     */
     public int getCoachingSkill() {
         return coachingSkill;
     }
 
+    /**
+     * Sets the coach's skill.
+     * @param coachingSkill skill value, must be in [0, 100]
+     */
     public void setCoachingSkill(int coachingSkill) {
         if (coachingSkill < 0 || coachingSkill > 100) {
             System.out.println("Cannot set coachingSkill to: " + coachingSkill);
@@ -31,10 +61,17 @@ public class Coach extends Person {
         this.coachingSkill = coachingSkill;
     }
 
+    /**
+     * @return coach's favorite formation
+     */
     public Formation getFormation() {
         return formation;
     }
 
+    /**
+     * Sets the coach's favorite formation.
+     * @param formation the coach's favorite formation
+     */
     public void setFormation(Formation formation) {
         this.formation = formation;
     }
--- src/hr/fer/oop/lab3/FootballPlayer.java
+++ src/hr/fer/oop/lab3/FootballPlayer.java
@@ -1,17 +1,41 @@
 package hr.fer.oop.lab3;
 
+/**
+ * Class that describes a FootballPlayer
+ */
 public class FootballPlayer extends Person {
 
+    /**
+     * Player's skill. Values must be in the range [0, 100].
+     */
     private int playingSkill;
 
+    /**
+     * Player's position on the football pitch.
+     */
     private PlayingPosition playingPosition;
 
+    /**
+     * Constructs a football player with the specified name, country and playing position.
+     * emotion and playingSkill are set to default values. (50, 50)
+     * @param name player's name
+     * @param country player's country
+     * @param playingPosition player's position on the pitch
+     */
     public FootballPlayer(String name, String country, PlayingPosition playingPosition) {
         super(name, country);
         this.playingSkill = 50;
         this.playingPosition = playingPosition;
     }
 
+    /**
+     * Constructs a football player with the specified name, country, playing position, emotion and playing skill.
+     * @param name player's name
+     * @param country player's country
+     * @param playingPosition player's position on the pitch
+     * @param emotion player's emotion
+     * @param playingSkill player's skill
+     */
     public FootballPlayer(String name, String country, PlayingPosition playingPosition, int emotion, int playingSkill) {
         super(name, country, emotion);
         this.playingSkill = 50;
@@ -19,10 +43,17 @@ public class FootballPlayer extends Person {
         this.playingPosition = playingPosition;
     }
 
+    /**
+     * @return player's skill
+     */
     public int getPlayingSkill() {
         return playingSkill;
     }
 
+    /**
+     * Sets the player's skill.
+     * @param playingSkill player's skill, must be in [0, 100]
+     */
     public void setPlayingSkill(int playingSkill) {
         if (playingSkill < 0 || playingSkill > 100) {
             System.out.println("Cannot set playingSkill to: " + playingSkill);
@@ -31,10 +62,17 @@ public class FootballPlayer extends Person {
         this.playingSkill = playingSkill;
     }
 
+    /**
+     * @return player's position on the pitch
+     */
     public PlayingPosition getPlayingPosition() {
         return playingPosition;
     }
 
+    /**
+     * Sets the player's position on the pitch.
+     * @param playingPosition player's position on the pitch
+     */
     public void setPlayingPosition(PlayingPosition playingPosition) {
         this.playingPosition = playingPosition;
     }
