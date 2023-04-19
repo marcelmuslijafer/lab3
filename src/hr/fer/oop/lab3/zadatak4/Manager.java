b635bc568df5f228f571f0964c2d49369102f7df
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-19 20:44:20 +0200

    Add Manager interface

---
+++ src/hr/fer/oop/lab3/zadatak4/Manager.java
@@ -0,0 +1,28 @@
+package hr.fer.oop.lab3.zadatak4;
+
+import hr.fer.oop.lab3.zadatak3.ManageableTeam;
+
+/**
+ * A coach knows how to manage a team if he implements this interface.
+ */
+public interface Manager {
+
+    /**
+     * Sets the team's formation to the coach's favorite.
+     */
+    void forceMyFormation();
+
+    /**
+     * Selects the starting eleven. All the selected players must be able
+     * to play in the team's formation. For example, if the formation
+     * is 4-4-2, the starting eleven must have a goalkeeper, 4 defenders,
+     * 4 midfielders and 2 attackers.
+     */
+    void pickStartingEleven();
+
+    /**
+     * Sets the manager's team.
+     * @param team the team to be managed
+     */
+    void setManagingTeam(ManageableTeam team);
+}
