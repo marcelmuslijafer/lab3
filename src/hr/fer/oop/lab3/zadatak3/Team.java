49ae4164ffc447dd792679176b69c261e4ee5850
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-19 13:16:19 +0200

    Implement ManagableTeam

--- src/hr/fer/oop/lab3/zadatak3/Team.java
+++ src/hr/fer/oop/lab3/zadatak3/Team.java
@@ -1,5 +1,6 @@
 package hr.fer.oop.lab3.zadatak3;
 
+import hr.fer.oop.lab3.zadatak1.FootballPlayer;
 import hr.fer.oop.lab3.zadatak1.Formation;
 import hr.fer.oop.lab3.zadatak2.SimpleFootballPlayerCollection;
 import hr.fer.oop.lab3.zadatak2.SimpleFootballPlayerCollectionImpl;
@@ -8,7 +9,7 @@ import hr.fer.oop.lab3.zadatak2.SimpleFootballPlayerCollectionImpl;
  * An abstract class that describes a Team.
  * Contains methods and fields common for all types of teams.
  */
-public abstract class Team {
+public abstract class Team implements ManageableTeam {
 
     /**
      * Name of the team
@@ -51,6 +52,48 @@ public abstract class Team {
      */
     public Team(String name, int numberOfRegisteredPlayers, Formation formation) {
         this(name, numberOfRegisteredPlayers);
+        this.formation = formation;
+    }
+
+    @Override
+    public boolean addPlayerToStartingEleven(FootballPlayer player) {
+        if (!registeredPlayers.contains(player)) {
+            return false;
+        }
+
+        return startingEleven.add(player);
+    }
+
+    @Override
+    public void clearStartingEleven() {
+        startingEleven.clear();
+    }
+
+    @Override
+    public Formation getFormation() {
+        return formation;
+    }
+
+    @Override
+    public SimpleFootballPlayerCollection getRegisteredPlayers() {
+        return registeredPlayers;
+    }
+
+    @Override
+    public SimpleFootballPlayerCollection getStartingEleven() {
+        return startingEleven;
+    }
+
+    @Override
+    public boolean isPlayerRegistered(FootballPlayer player) {
+        return registeredPlayers.contains(player);
+    }
+
+    @Override
+    public void setFormation(Formation formation) {
+        if (formation == null)
+            return;
+
         this.formation = formation;
     }
 }
