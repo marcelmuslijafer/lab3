5a36549be4e670adade0bf71681accc8774673dd
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-19 21:28:09 +0200

    Finish task 4

--- src/hr/fer/oop/lab3/zadatak1/Coach.java
+++ src/hr/fer/oop/lab3/zadatak1/Coach.java
@@ -1,9 +1,14 @@
 package hr.fer.oop.lab3.zadatak1;
 
+import hr.fer.oop.lab3.zadatak2.SimpleFootballPlayerCollection;
+import hr.fer.oop.lab3.zadatak2.SimpleFootballPlayerCollectionImpl;
+import hr.fer.oop.lab3.zadatak3.ManageableTeam;
+import hr.fer.oop.lab3.zadatak4.Manager;
+
 /**
  * Class that describes a Coach.
  */
-public class Coach extends Person {
+public class Coach extends Person implements Manager {
 
     /**
      * Coach's skill. Values must be in the range [0, 100].
@@ -14,6 +19,11 @@ public class Coach extends Person {
      * Coach's favorite formation to play.
      */
     private Formation formation;
+
+    /**
+     * The coach's team.
+     */
+    private ManageableTeam managingTeam;
 
     /**
      * Constructs a coach with the specified name and country.
@@ -40,6 +50,73 @@ public class Coach extends Person {
         this.coachingSkill = 50;
         setCoachingSkill(coachingSkill);
         this.formation = formation;
+    }
+
+    @Override
+    public void forceMyFormation() {
+        managingTeam.setFormation(formation);
+    }
+
+    @Override
+    public void pickStartingEleven() {
+        int registeredPlayersCount = managingTeam.getRegisteredPlayers().size();
+        FootballPlayer[] players = managingTeam.getRegisteredPlayers().getPlayers();
+        Formation teamFormation = managingTeam.getFormation();
+
+        SimpleFootballPlayerCollection goalkeepers = new SimpleFootballPlayerCollectionImpl(1);
+        SimpleFootballPlayerCollection defenders = new SimpleFootballPlayerCollectionImpl(teamFormation.getDf());
+        SimpleFootballPlayerCollection midfielders = new SimpleFootballPlayerCollectionImpl(teamFormation.getMf());
+        SimpleFootballPlayerCollection forwards = new SimpleFootballPlayerCollectionImpl(teamFormation.getFw());
+
+        for (int i = 0; i < registeredPlayersCount; i++) {
+            FootballPlayer player = players[i];
+            switch (player.getPlayingPosition()) {
+                case GK -> goalkeepers.add(player);
+                case DF -> defenders.add(player);
+                case MF -> midfielders.add(player);
+                case FW -> forwards.add(player);
+            }
+        }
+
+        String format = "Not enough %s in the team to play " + teamFormation + ".";
+        if (goalkeepers.size() < goalkeepers.getMaxSize()) {
+            System.out.printf(format, "goalkeepers");
+            return;
+        }
+        if (defenders.size() < defenders.getMaxSize()) {
+            System.out.printf(format, "defenders");
+            return;
+        }
+        if (midfielders.size() < midfielders.getMaxSize()) {
+            System.out.printf(format, "midfielders");
+            return;
+        }
+        if (forwards.size() < forwards.getMaxSize()) {
+            System.out.printf(format, "forwards");
+            return;
+        }
+
+        managingTeam.getStartingEleven().clear();
+        for (FootballPlayer goalkeeper : goalkeepers.getPlayers()) {
+            managingTeam.addPlayerToStartingEleven(goalkeeper);
+        }
+        for (FootballPlayer defender : defenders.getPlayers()) {
+            managingTeam.addPlayerToStartingEleven(defender);
+        }
+        for (FootballPlayer midfielder : midfielders.getPlayers()) {
+            managingTeam.addPlayerToStartingEleven(midfielder);
+        }
+        for (FootballPlayer forward : forwards.getPlayers()) {
+            managingTeam.addPlayerToStartingEleven(forward);
+        }
+    }
+
+    @Override
+    public void setManagingTeam(ManageableTeam team) {
+        if (team == null)
+            return;
+
+        managingTeam = team;
     }
 
     /**
--- src/hr/fer/oop/lab3/zadatak1/Formation.java
+++ src/hr/fer/oop/lab3/zadatak1/Formation.java
@@ -5,20 +5,42 @@ package hr.fer.oop.lab3.zadatak1;
  */
 public class Formation {
 
+    private final int df;
+    private final int mf;
+    private final int fw;
+
+    private Formation(int df, int mf, int fw) {
+        this.df = df;
+        this.mf = mf;
+        this.fw = fw;
+    }
+
+    public int getDf() {
+        return df;
+    }
+
+    public int getMf() {
+        return mf;
+    }
+
+    public int getFw() {
+        return fw;
+    }
+
     /**
      * 4-4-2 formation
      */
-    private static Formation f442 = new Formation();
+    private static final Formation f442 = new Formation(4, 4, 2);
 
     /**
      * 3-5-2 formation
      */
-    private static Formation f352 = new Formation();
+    private static final Formation f352 = new Formation(3, 5, 2);
 
     /**
      * 5-4-1 formation
      */
-    private static Formation f541 = new Formation();
+    private static final Formation f541 = new Formation(5, 4, 1);
 
     /**
      * @return the 4-4-2 formation
