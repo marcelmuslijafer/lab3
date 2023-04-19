07c3f6df8139a44acd8a85c68de285b094644677
Marcel Muslija <marcel.muslija@gmail.com>
2023-04-19 12:51:48 +0200

    Add club and national team

---
+++ src/hr/fer/oop/lab3/zadatak3/ClubTeam.java
@@ -0,0 +1,52 @@
+package hr.fer.oop.lab3.zadatak3;
+
+import hr.fer.oop.lab3.zadatak1.Formation;
+
+/**
+ * Class that describes a football club.
+ */
+public class ClubTeam extends Team {
+
+    /**
+     * A football club can't have more than 25 players registered.
+     */
+    private static final int REGISTERED_CAPACITY = 25;
+
+    /**
+     * Football club's reputation.
+     */
+    private int reputation;
+
+    /**
+     * Constructs a football club with the specified name.
+     * Reputation and formation are set to default (50, 4-4-2).
+     * @param name club's name
+     */
+    public ClubTeam(String name) {
+        super(name, REGISTERED_CAPACITY);
+        this.reputation = 50;
+    }
+
+    /**
+     * Constructs a football club with the specified name and reputation.
+     * Formation is set to default (4-4-2).
+     * @param name club's name
+     * @param reputation club's reputation
+     */
+    public ClubTeam(String name, int reputation) {
+        super(name, REGISTERED_CAPACITY);
+        this.reputation = reputation;
+    }
+
+    /**
+     * Constructs a football club with the specified name, reputation and formation.
+     * @param name team's name
+     * @param reputation team's reputation
+     * @param formation team's formation
+     */
+    public ClubTeam(String name, int reputation, Formation formation) {
+        super(name, REGISTERED_CAPACITY, formation);
+        this.reputation = reputation;
+    }
+
+}
--- src/hr/fer/oop/lab3/zadatak3/NationalTeam.java
+++ src/hr/fer/oop/lab3/zadatak3/NationalTeam.java
@@ -2,23 +2,46 @@ package hr.fer.oop.lab3.zadatak3;
 
 import hr.fer.oop.lab3.zadatak1.Formation;
 
+/**
+ * Class that describes a football national team.
+ */
 public class NationalTeam extends Team {
 
-    private static final int MAX_REGISTERED_COUNT = 23;
+    /**
+     * National teams can't have more than 23 players registered.
+     */
+    private static final int REGISTERED_CAPACITY = 23;
 
-
+    /**
+     * National team's country.
+     */
     private String country;
 
+    /**
+     * Constructs a national team with the specified country.
+     * The name of the national team is set to country.
+     * Formation is set to default (4-4-2).
+     * @param country national team's country.
+     */
     public NationalTeam(String country) {
-        super(country, MAX_REGISTERED_COUNT);
+        super(country, REGISTERED_CAPACITY);
         this.country = country;
     }
 
+    /**
+     * Constructs a national team with the specified country and formation.
+     * The name of the national team is set to country.
+     * @param country national team's country
+     * @param formation national team's formation
+     */
     public NationalTeam(String country, Formation formation) {
-        super(country, MAX_REGISTERED_COUNT, formation);
+        super(country, REGISTERED_CAPACITY, formation);
         this.country = country;
     }
 
+    /**
+     * @return the national team's country
+     */
     public String getCountry() {
         return country;
     }
